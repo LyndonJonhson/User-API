c01a70e3f3dd25613bc1078835ab5645440859ed
Jonhson Cabral <jonhsoncabral13@gmail.com>
2022-03-27 21:14:23 -0300

    first commit

---
+++ middleware/AdminAuth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+var jwtSecret = "soasiasaklidfmkufumdklilasmalsa";
+
+module.exports = function(req, res, next){
+    const authToken = req.headers['authorization'];
+    if(authToken != undefined){
+        const bearer = authToken.split(" ");
+        var token = bearer[1];
+
+        try{
+            var decoded = jwt.verify(token, jwtSecret);
+            if(decoded.role == 1){
+                next();
+            }else{
+                res.status(403);
+                res.send("Você não tem permissão para isso");
+                return;
+            }            
+        }catch(err){
+            res.status(403);
+            res.send("Você não está autenticado");
+            return;
+        }
+
+    }else{
+        res.status(403);
+        res.send("Você não está autenticado");
+        return;
+    }
+}
